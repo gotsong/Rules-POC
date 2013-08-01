07e8614ca9cff359969d12fd9eb17732bbc6d966
gotsong <gotsong@me.com>
2013-07-31 22:31:59 -0500

    Initial project creation

---
+++ ExternalRuleSetHandler/RuleSetHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Workflow.Activities.Rules;
+using System.Workflow.ComponentModel.Serialization;
+using Microsoft.Samples.Rules.ExternalRuleSetLibrary;
+using Microsoft.Samples.Rules.ExternalRuleSetService;
+
+namespace ExternalRuleSetHandler
+{
+    public class RuleSetHandler
+    {
+        private string _ruleSetName;
+        private RuleSet _ruleSet;
+
+        public void LoadRuleSet(string ruleSetName)
+        {
+            var serializer = new WorkflowMarkupSerializer();
+            if (string.IsNullOrEmpty(ruleSetName))
+                throw new Exception("Ruleset name cannot be null or empty.");
+            if (!string.Equals(RuleSetName, ruleSetName))
+            {
+                _ruleSetName = ruleSetName;
+
+                var ruleService = new ExternalRuleSetService();
+                var ruleSet = ruleService.GetRuleSet(new RuleSetInfo(ruleSetName));
+                if (ruleSet == null)
+                {
+                    throw new Exception("RuleSet could not be loaded. Make sure the connection string and ruleset name are correct.");
+                }
+            }
+        }
+
+        public string RuleSetName
+        {
+            get { return _ruleSetName; }
+            set { _ruleSetName = value; }
+        }
+
+        public RuleSet RuleSet
+        {
+            get { return _ruleSet; }
+        }
+    }
+}
---
+++ ExternalRuleSetHandler/RuleSetValidation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Workflow.Activities.Rules;
+using System.Workflow.ComponentModel.Compiler;
+
+namespace ExternalRuleSetHandler
+{
+    public class RuleSetValidation
+    {
+        private Type _targetType;
+
+        public RuleSetValidation(Type targetType)
+        {
+            _targetType = targetType;
+        }
+
+        private RuleExecution ValidateRuleSet(object targetObject)
+        {
+            RuleValidation ruleValidation;
+
+            ruleValidation = new RuleValidation(_targetType, null);
+            if (!ruleSet.Validate(ruleValidation))
+            {
+                string errors = "";
+                foreach (ValidationError validationError in ruleValidation.Errors)
+                    errors = errors + validationError.ErrorText + "\n";
+                Debug.WriteLine("Validation Errors \n" + errors);
+                return null;
+            }
+            else
+            {
+                return new RuleExecution(ruleValidation, targetObject);
+            }
+        }         
+    }
+}
